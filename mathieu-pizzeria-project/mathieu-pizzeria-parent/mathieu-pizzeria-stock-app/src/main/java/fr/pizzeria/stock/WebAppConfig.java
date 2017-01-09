6e8c297125de1ae630c7f2dcc3b6ace2fbe1536d
Mathieu Rousseau <Mathieu Rousseau>
2017-01-09 22:41:44 +0100

    ajout de modification pour le tp 7

--- mathieu-pizzeria-project/mathieu-pizzeria-parent/mathieu-pizzeria-stock-app/src/main/java/fr/pizzeria/stock/WebAppConfig.java
+++ mathieu-pizzeria-project/mathieu-pizzeria-parent/mathieu-pizzeria-stock-app/src/main/java/fr/pizzeria/stock/WebAppConfig.java
@@ -1,17 +1,25 @@
 package fr.pizzeria.stock;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import fr.pizzeria.dao.pizza.PizzaDao;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("fr.pizzeria")
 public class WebAppConfig {
-
+	
 	@Bean
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -21,4 +29,20 @@ public class WebAppConfig {
         return viewResolver;
 	}
 	
+	@Bean
+    public PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager();
+    }
+
+    @Bean
+    public PersistenceAnnotationBeanPostProcessor beanPostProcessor() {
+        return new PersistenceAnnotationBeanPostProcessor();
+    }
+
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean entityManagerFactoryBean = new LocalEntityManagerFactoryBean();
+        entityManagerFactoryBean.setPersistenceUnitName("mathieu-h2");
+        return entityManagerFactoryBean;
+    }
 }
